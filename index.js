b4859044484abbf10e519b0c0678d15e2190316f
unkelpehr <unkelpehr@gmail.com>
2017-09-25 15:38:50 +0200

    expose Promise provider

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+exports.Promise = require('bluebird');
+
 exports.Database = require('./lib/Database');
 exports.Collection = require('./lib/Collection');
 exports.Document = require('./lib/Document');
--- test/basics.js
+++ test/basics.js
@@ -2,7 +2,7 @@ const {test} = require('ava');
 const mongorite = require('../');
 
 test('exposed properties', assert => {
-	const expect = ['Database', 'Collection', 'Document'].sort();
+	const expect = ['Database', 'Collection', 'Document', 'Promise'].sort();
 	const actual = Object.keys(mongorite).sort();
 
 	assert.deepEqual(expect, actual);
