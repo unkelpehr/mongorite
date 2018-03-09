e8fa79108eeb8cc8111e81a01e4bc42fa4c23f91
Pehr Boman <unkelpehr@gmail.com>
2018-03-09 11:25:23 +0100

    Bug

--- dev/index.js
+++ dev/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* eslint-disable */
+
 const {Database, Collection, Document, plugins, ObjectId, Promise, common} = require('../index');
 
 const UserDocument = require('./UserDocument');
@@ -41,247 +43,34 @@ function getTestData () {
 }
 
 const db = new Database('localhost/mongorite_test');
-
-//Collection.use(plugins.runtime);
-Collection.use(plugins.schemas({
-	allErrors: true,
-	before: 'save',
-	verbose: true
-}));
-
 const users = new UserCollection(db);
 const user = new UserDocument(users);
-const user2 = users.createDocument({foo: 'bar'});
 
 (async function () {
 	await db.connect();
-	await user2.save();
-	await db.disconnect();
-}());
 
+	const user = await users.query.findById(ObjectId("5aa255191f3a6742981f605f"));
+	// const user = users.createDocument();
 
-return;
-const data = {
-	a: 'b',
-	c: {
-		d: 'e'
-	},
-	f: {
-		g: {
-			h: {
-				i: 'j'
-				}
-			}
-		},
-	k: 'l',
-	m: {
-		n: {
-			o: {
-				p: 'q',
-				r: 's'
-			}
-		}
-	},
-	t: null,
-	u: 'v',
-	w: {
-		x: 'y',
-		z: {}
-	}
-};
-
-function hide (obj, key, val) {
-	Object.defineProperty(obj, key, {
-		enumerable: false,
-		configurable: true,
-		writable: true,
-		value: val
-	});
-
-	return obj;
-}
-
-return (function () {
-	const doc = new Document();
-
-	doc.set(data);
-
-	inspect(doc);
-}());
-
-return (function () {
-	const Benchmark = require('Benchmark');
-	const suite = new Benchmark.Suite;
-	 
-	suite.add('set();', function() {
-		user.set(data);
-	});
-
-	suite.add('set2()', function() {
-		user.set2(data);
-	});
-
-	suite.on('cycle', function(event) {
-		console.log(String(event.target));
-	}).on('complete', function() {
-		console.log('Fastest is ' + this.filter('fastest').map('name'));
-	});
-	
-	suite.run({ 'async': true });
-}());
-
- (function () {
-	const target = {};
-	const source = {
-		a: 'b',
-		c: {
-			d: 'e'
-		},
-		f: {
-			g: {
-				h: {
-					i: 'j'
-					}
-				}
-			},
-		k: 'l',
-		m: {
-			n: {
-				o: {
-					p: 'q',
-					r: 's'
-				}
-			}
-		},
-		t: null,
-		u: 'v',
-		w: {
-			x: 'y',
-			z: {}
-		}
-	};
-
-	transform(target, source);
-
-	const source2 = {
-		a: 'not b',
-		'c.d': 'not e'
-	}
-
-	transform(target, source2);
-
-	inspect(target);
-}());
-
-(function () {
-	const Benchmark = require('Benchmark');
-	const suite = new Benchmark.Suite;
-	 
-	suite.add('set();', function() {
-		user.set(data);
-	});
-
-	suite.add('set2()', function() {
-		user.set2(data);
-	});
-
-	suite.on('cycle', function(event) {
-		console.log(String(event.target));
-	}).on('complete', function() {
-		console.log('Fastest is ' + this.filter('fastest').map('name'));
-	});
-	
-	suite.run({ 'async': true });
-}())
-
-return (function () {
-	//await db.connect();
-
-	const col = new UserCollection(db);
-	const doc = new UserDocument(col);
-	
-	const obj1 = {
-		k1: 'v1',
-		k2: {},
-		k3: {
-			l1k1: {
-				l2k1: 'l2v1'
-			}
-		},
-		k4: {
-			l1k1: {
-				l2k1: 'l2v1',
-				l2k2: 'l2v2'
-			}
-		}
-	};
-
-	doc.set('foo', 'bar');
-	doc.set({'foo': 'bar'});
-
-	doc.set(doc.CONST.SET_WRITE, 'foo', 'bar');
-	doc.set(doc.CONST.SET_MERGE_WRITE, {'foo': 'bar'});
-
-	//inspect(doc.get('k3'));
-
-	//db.disconnect();
-}());
-
-user.set({foo: {bar: ''}});
-
-user.set('foo.bar', 'qux');
-return;
-
-(function () {
-	const Benchmark = require('Benchmark');
-	const suite = new Benchmark.Suite;
-	 
-	suite.add('set();', function() {
-		user.set(data);
-	});
-
-	suite.add('set2()', function() {
-		user.set2(data);
-	});
-
-	suite.on('cycle', function(event) {
-		console.log(String(event.target));
-	}).on('complete', function() {
-		console.log('Fastest is ' + this.filter('fastest').map('name'));
-	});
-	
-	suite.run({ 'async': true });
-}())
-
-
-return;
-async function tests () {
-	await db.connect();
-
-	const users = new UserCollection(db);
-	const user = new UserDocument(users);
+	// user.set('lvl1.bla1', true);
+	// user.set('lvl1.bla2', false);
 
 	user.set({
-		first_name: 'Flash',
-		last_name: 'Gordon'
+		lvl1: {
+			bla1: Math.random()
+		},
+
+		lvl2: {
+			bla2: Math.random()
+		}
 	});
 
-	users.push(user);
-
+	user.set('mittdatum', new Date());
 	await user.save();
 
-	user.set({
-		first_name: 'Flashy',
-		department: 'Justice'
-	});
-
-	user.inspect();
-
 	await db.disconnect();
-}
-
-tests().catch(err => {
+}()).catch(err => {
 	console.log('Unhandled Promise Rejection');
 	inspect(err);
 	process.exit();
-});
+});;
